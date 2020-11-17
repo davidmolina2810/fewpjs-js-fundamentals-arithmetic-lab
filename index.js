c2f04a5097635d3456a32843211610168fb661c9
davidmolina2810 <davidmolina2810@gmail.com>
2020-11-17 09:45:01 -0600

    commit tree fab4d1ded21fe757c4549011d388ec9e9f818a62

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+const num1 = 6.2 
+const num2 = 10
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let mod = 16 % 6
+
+let max = Math.max(10, 5, 20)
